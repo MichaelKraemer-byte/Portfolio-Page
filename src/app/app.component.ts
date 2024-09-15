8f60908dea9d41ca6efc404a5b4f71375592e99c
Michael Krämer <info.m.kraemer@gmail.com>
2024-09-16 01:38:34 +0200

    first commit - angular, components and atf section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    TestimonialsComponent,
+    ContactFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
