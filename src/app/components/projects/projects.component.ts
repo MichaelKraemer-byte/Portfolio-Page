df74ed3448682a49d51bbc9500356da42640ecc0
Michael Krämer <info.m.kraemer@gmail.com>
2024-09-22 12:56:00 +0200

    beginning of projects section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  isHiddenProjectVisible1 = false;
+  isHiddenProjectVisible2 = false;
+  isHiddenProjectVisible3 = false;
 
+//ng style visible anstatt ngif 
+  showHiddenProject1() {
+    this.isHiddenProjectVisible1 = true;
+  }
+
+  showHiddenProject2() {
+    this.isHiddenProjectVisible2 = true;
+  }
+
+  showHiddenProject3() {
+    this.isHiddenProjectVisible3 = true;
+  }
+
+  hideHiddenProject1() {
+    this.isHiddenProjectVisible1 = false;
+  }
+
+  hideHiddenProject2() {
+    this.isHiddenProjectVisible2 = false;
+  }
+
+  hideHiddenProject3() {
+    this.isHiddenProjectVisible3 = false;
+  }
 }
